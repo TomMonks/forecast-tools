5a57da54b4525e8716bc4ed788a79b872f5429a8
TomMonks <thomas.m.monks@gmail.com>
2020-05-10 09:50:34 +0100

    Snaive failing new test for numpy input

--- tests/test_naive.py
+++ tests/test_naive.py
@@ -158,7 +158,55 @@ def test_naive1_forecast_input_numpy(data, horizon, expected):
     test the correct number of error metric functions are returned.
     '''
     model = b.Naive1()
+    model.fit(np.array(data))
+    #point forecasts only
+    preds = model.predict(horizon)
+    assert len(preds) == expected
+
+
+@pytest.mark.parametrize("data, horizon, expected", 
+                         [([1, 2, 3, 4, 5], 12, 12),
+                          ([1, 2, 3, 4, 5], 24, 24),
+                          ([1, 2, 3], 8, 8)
+                          ])
+def test_snaive_forecast_input_series(data, horizon, expected):
+    '''
+    test the correct number of error metric functions are returned.
+    '''
+    model = b.SNaive(1)
     model.fit(pd.Series(data))
+    #point forecasts only
+    preds = model.predict(horizon)
+    assert len(preds) == expected
+
+
+@pytest.mark.parametrize("data, horizon, expected", 
+                         [([1, 2, 3, 4, 5], 12, 12),
+                          ([1, 2, 3, 4, 5], 24, 24),
+                          ([1, 2, 3], 8, 8)
+                          ])
+def test_snaive_forecast_input_dataframe(data, horizon, expected):
+    '''
+    test the correct number of error metric functions are returned.
+    '''
+    model = b.SNaive(1)
+    model.fit(pd.DataFrame(data))
+    #point forecasts only
+    preds = model.predict(horizon)
+    assert len(preds) == expected
+
+
+@pytest.mark.parametrize("data, horizon, expected", 
+                         [([1, 2, 3, 4, 5], 12, 12),
+                          ([1, 2, 3, 4, 5], 24, 24),
+                          ([1, 2, 3], 8, 8)
+                          ])
+def test_snaive_forecast_input_numpy(data, horizon, expected):
+    '''
+    test the correct number of error metric functions are returned.
+    '''
+    model = b.SNaive(1)
+    model.fit(np.array(data))
     #point forecasts only
     preds = model.predict(horizon)
     assert len(preds) == expected
