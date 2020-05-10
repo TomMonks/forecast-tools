62344cba5323fd28df70620c0872e0e34aa97ff5
TomMonks <thomas.m.monks@gmail.com>
2020-05-10 09:47:19 +0100

    added tests for numpy, pd.series and pd.Dataframe inputs to Naive1

--- tests/test_naive.py
+++ tests/test_naive.py
@@ -78,7 +78,7 @@ def test_average_forecast_horizon(data, horizon, expected):
                           ])
 def test_average_forecast_input_numpy(data, horizon, expected):
     '''
-    test the correct number of error metric functions are returned.
+    test the average class accepts numpy array
     '''
     model = b.Average()
     model.fit(np.array(data))
@@ -93,7 +93,7 @@ def test_average_forecast_input_numpy(data, horizon, expected):
                           ])
 def test_average_forecast_input_series(data, horizon, expected):
     '''
-    test the correct number of error metric functions are returned.
+    test the average class accepts pandas series
     '''
     model = b.Average()
     model.fit(pd.Series(data))
@@ -108,10 +108,57 @@ def test_average_forecast_input_series(data, horizon, expected):
                           ])
 def test_average_forecast_input_dataframe(data, horizon, expected):
     '''
-    test the correct number of error metric functions are returned.
+    test the average baseline class accept dataframe
     '''
     model = b.Average()
     model.fit(pd.DataFrame(data))
+    #point forecasts only
+    preds = model.predict(horizon)
+    assert len(preds) == expected
+
+
+@pytest.mark.parametrize("data, horizon, expected", 
+                         [([1, 2, 3, 4, 5], 12, 12),
+                          ([1, 2, 3, 4, 5], 24, 24),
+                          ([1, 2, 3], 8, 8)
+                          ])
+def test_naive1_forecast_input_dataframe(data, horizon, expected):
+    '''
+    test the correct number of error metric functions are returned.
+    '''
+    model = b.Naive1()
+    model.fit(pd.DataFrame(data))
+    #point forecasts only
+    preds = model.predict(horizon)
+    assert len(preds) == expected
+
+
+@pytest.mark.parametrize("data, horizon, expected", 
+                         [([1, 2, 3, 4, 5], 12, 12),
+                          ([1, 2, 3, 4, 5], 24, 24),
+                          ([1, 2, 3], 8, 8)
+                          ])
+def test_naive1_forecast_input_series(data, horizon, expected):
+    '''
+    test the correct number of error metric functions are returned.
+    '''
+    model = b.Naive1()
+    model.fit(pd.Series(data))
+    #point forecasts only
+    preds = model.predict(horizon)
+    assert len(preds) == expected
+
+@pytest.mark.parametrize("data, horizon, expected", 
+                         [([1, 2, 3, 4, 5], 12, 12),
+                          ([1, 2, 3, 4, 5], 24, 24),
+                          ([1, 2, 3], 8, 8)
+                          ])
+def test_naive1_forecast_input_numpy(data, horizon, expected):
+    '''
+    test the correct number of error metric functions are returned.
+    '''
+    model = b.Naive1()
+    model.fit(pd.Series(data))
     #point forecasts only
     preds = model.predict(horizon)
     assert len(preds) == expected
