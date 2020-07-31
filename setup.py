d3784167d1d04d846cccf7411f1d4e8af36eb72d
Tom Monks <thomas.m.monks@gmail.com>
2020-07-31 14:15:22 +0100

    added pip requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
 
+# Read in the requirements.txt file
+with open("requirements.txt") as f:
+    requirements = []
+    for library in f.read().splitlines():
+        requirements.append(library)
+
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="forecast-tools", 
-    version="0.1.2",
+    version="0.1.3",
     author="Thomas Monks",
     author_email="forecast_tools@gmail.com",
     license="The MIT License (MIT)",
@@ -16,9 +23,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=requirements,
 )
