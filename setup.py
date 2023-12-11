ca76349a98050b93b6200b1600c010f8a4419cfc
TomMonks <thomas.m.monks@gmail.com>
2023-12-11 18:22:19 +0000

    SETUP: updated python version meta data -> 3.10+3.11

--- setup.py
+++ setup.py
@@ -41,12 +41,11 @@ setuptools.setup(
     include_package_data=True,
     package_data={"": ["forecast_tools/data/*.csv"]},
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6.9',
+    python_requires='>=3.10',
     install_requires=requirements,
 )
