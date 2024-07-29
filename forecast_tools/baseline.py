faedbafa046bb2dfd71fd13fabf2e131d5869cec
TomMonks <thomas.m.monks@gmail.com>
2024-07-29 17:07:04 +0100

    fix(baseline): np.int -> int; np.float -> float. this was depr error

--- forecast_tools/baseline.py
+++ forecast_tools/baseline.py
@@ -11,7 +11,7 @@ Naive1:
 SNaive:
     Carry forward value from last seasonal period
 
-Average: np.sqrt(((h - 1) / self._period).astype(np.int)+1)
+Average: np.sqrt(((h - 1) / self._period).astype(int)+1)
     Carry forward average of observations
 
 Drift:
@@ -433,7 +433,7 @@ class Naive1(Forecast):
 
         std = np.full(shape=horizon,
                       fill_value=self._resid_std,
-                      dtype=np.float)
+                      dtype=float)
 
         std_h = std * indexes
         return std_h
@@ -593,8 +593,9 @@ class SNaive(Forecast):
     def _std_h(self, horizon):
         h = np.arange(1, horizon+1)
         # need to query if should be +1 or not.
+        # modified v0.3.0 np.int -> int
         return self._resid_std * \
-            np.sqrt(((h - 1) / self._period).astype(np.int)+1)
+            np.sqrt(((h - 1) / self._period).astype(int)+1)
 
 
 class Average(Forecast):
@@ -745,7 +746,8 @@ class Average(Forecast):
 
     def _std_h(self, horizon):
         std = self._resid_std * np.sqrt(1 + (1/self._t))
-        return np.full(shape=horizon, fill_value=std, dtype=np.float)
+        # modified v0.3.0 np.float -> float
+        return np.full(shape=horizon, fill_value=std, dtype=float)
 
 
 class Drift(Forecast):
